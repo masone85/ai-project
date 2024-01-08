e943d6222603748242938c00bc14a8836413dc14
masone85 <113108800+masone85@users.noreply.github.com>
2024-01-08 13:22:21 +0000

    initial set up and add api key, axios, prompt testing etc.

---
+++ index.js
@@ -0,0 +1,15 @@
+function showAnswer(response) {
+    console.log(response);
+}
+
+
+
+
+
+let apiKey = "af8a02404a8co87d6b9be4abdat1baf3";
+let context = "be polite and provide a very short answer";
+let prompt = "when was the first AI created";
+let apiUrl =
+  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+axios.get(apiUrl).then(showAnswer)
