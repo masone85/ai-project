c3c065f1a045f4d27130a91ef1c94e4620c63344
masone85 <113108800+masone85@users.noreply.github.com>
2024-01-08 18:19:48 +0000

    add media

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function showAnswer(response) {
-  console.log(response);
+  //alert(response.data.answer);
 }
 
 let apiKey = "af8a02404a8co87d6b9be4abdat1baf3";
 let context = "be polite and provide a very short answer";
-let prompt = "who is matt delac";
+let prompt = "who was the first female president";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showAnswer);
